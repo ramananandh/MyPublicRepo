2be129959605f69e338c9fd9ace6a196a0357d7c
Vivek <vthangathurai@ebay.com>
2011-09-04 21:42:07 -0700

    deleting targer,bin,settings folder

--- codegen/codegen-tools/target/test-classes/CalculatorServiceImpl/gen-src/com/ebayopensource/test/soaframework/tools/codegen/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://codegen.tools.soaframework.test.ebayopensource.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.ebayopensource.test.soaframework.tools.codegen;
--- codegen/turmeric-maven-plugin/bin/src/test/resources/standard/README.txt
+++
@@ -1,4 +0,0 @@
-Standard Projects are projects that conform to the Maven Project Standards
-for layout, and mojo configuration.
-
-Do not include the ${basedir}/service*.properties files.
